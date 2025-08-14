cb47d5e620ff76d1743a198f3727a93ead16fe5b
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-08-15 00:18:28 +0530

    Api Fixing

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const app = express();
-app.use(express.json())
-require('./connection')
-app.get('/', (req, res)=>{
-res.send({message: "Successfully Running "})
-})
-app.listen(4000, () => {
-console.log("Successfully running on port 4000")
-})
+const cookieparser = require('cookie-parser');
+const cors = require("cors")
 
+require('dotenv').config({path:"./.env"})
+
+
+app.use(express.json())
+app.use(cookieparser())
+
+app.use(cors({
+    credentials:true,
+    origin:"*"
+}));
+
+require('./connection');
+
+const userRoutes = require('./Routes/user');
+const facilityRoutes = require('./Routes/facility')
+const medicineRoutes = require('./Routes/medicine')
+const hopitalRoutes = require('./Routes/nearByHospital')
+const notificationRoutes = require('./Routes/notification');
+const gallaryRoutes = require('./Routes/gallary');
+const historyRoutes = require('./Routes/history')
+
+app.use("/api/auth",userRoutes)
+app.use("/api/facility",facilityRoutes)
+app.use("/api/medicine",medicineRoutes)
+app.use("/api/hospital",hopitalRoutes)
+app.use('/api/notification',notificationRoutes)
+app.use('/api/gallary',gallaryRoutes)
+app.use('/api/history',historyRoutes)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("Successfully running on port",process.env.PORT)
+})
