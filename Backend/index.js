48b278d72dfb75b8ddbabee352e508a415348f58
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-08-21 22:42:00 +0530

    s

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
     credentials:true,
-    origin:"http://localhost:5173"   // or your frontend URL
+    origin:"https://collagedispensary-tau.vercel.app/3"   // or your frontend URL
 }));
 
 require('./connection');
