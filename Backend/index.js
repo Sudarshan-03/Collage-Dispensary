926c79f5d5e9822f4b4a4b7678ee227e76d6f282
Sudarshan-03 <sudarshan2004yadav@gmail.com>
2025-08-21 23:50:35 +0530

    FE link

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:3000",
-   "https://collagedispensary-tau.vercel.app"
+   "https://dispensary-snowy.vercel.app/"
   ],
   credentials: true
 }));
